18213a4256558b0cdb3cff1256d9e191e746faf4
Matus <matuhrin@gmail.com>
2020-10-21 19:21:55 +0200

    Commented LED section

--- led.cpp
+++ led.cpp
@@ -13,38 +13,38 @@
 // example for more information on possible values.
 //Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, RED_LED_HAND, NEO_GRB + NEO_KHZ800);
 
-void LEDsSetup() {
-  pixels.begin(); // This initializes the NeoPixel library.
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if (WheelPos < 85) {
-    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if (WheelPos < 170) {
-    WheelPos -= 85;
-    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
-
-void SetLEDs() {
-  static uint16_t i, j = 0;
-  j = !j;
-  if (j)
-  {
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-  }
-  else
-  {
-     pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-      i++;
-  }
-  pixels.show();
- 
-  if (i == 4) i = 0;
-}
+//void LEDsSetup() {
+//  pixels.begin(); // This initializes the NeoPixel library.
+//}
+//
+//// Input a value 0 to 255 to get a color value.
+//// The colours are a transition r - g - b - back to r.
+//uint32_t Wheel(byte WheelPos) {
+//  WheelPos = 255 - WheelPos;
+//  if (WheelPos < 85) {
+//    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+//  }
+//  if (WheelPos < 170) {
+//    WheelPos -= 85;
+//    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+//  }
+//  WheelPos -= 170;
+//  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+//}
+//
+//void SetLEDs() {
+//  static uint16_t i, j = 0;
+//  j = !j;
+//  if (j)
+//  {
+//    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+//  }
+//  else
+//  {
+//     pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+//      i++;
+//  }
+//  pixels.show();
+// 
+//  if (i == 4) i = 0;
+//}
